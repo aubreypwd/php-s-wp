6a457cf96c97f9ea3e9eb0ef6c9c7b52f64e991c
Aubrey Portwood <code@aubreypwd.com>
2022-06-29 11:14:05 -0600

    Improvments for how it works.
    Initial commit wasn't actually working because the __DIR__ was
    different.

--- src/directory-slash-fix.php
+++ src/directory-slash-fix.php
@@ -8,21 +8,49 @@
  * @since 1.0.0
  */
 
+if ( defined( 'WP_CLI' ) ) {
+
+	// Ignore WP CLI.
+	return;
+}
+
+// Yes, put these in a place where they won't be in the way of WordPress.
+$_GLOBALS['php-s-wp']['directory-slash-fix']['parsed']    = parse_url( isset( $_SERVER["REQUEST_URI"] ) ? $_SERVER["REQUEST_URI"] : '' );
+$_GLOBALS['php-s-wp']['directory-slash-fix']['query']     = isset( $_GLOBALS['php-s-wp']['directory-slash-fix']['parsed']['query'] ) ? "{$_GLOBALS['php-s-wp']['directory-slash-fix']['parsed']['query']}" : '';
+$_GLOBALS['php-s-wp']['directory-slash-fix']['path']      = isset( $_GLOBALS['php-s-wp']['directory-slash-fix']['parsed']['path'] ) ? "{$_GLOBALS['php-s-wp']['directory-slash-fix']['parsed']['path']}" : '';
+$_GLOBALS['php-s-wp']['directory-slash-fix']['doc_root']  = isset( $_SERVER['DOCUMENT_ROOT'] ) ? $_SERVER['DOCUMENT_ROOT'] : '';
+$_GLOBALS['php-s-wp']['directory-slash-fix']['real_path'] = rtrim( $_GLOBALS['php-s-wp']['directory-slash-fix']['doc_root'], '/' ) . '/' . ltrim( $_GLOBALS['php-s-wp']['directory-slash-fix']['path'], '/' );
+
+if (
+	empty( $_GLOBALS['php-s-wp']['directory-slash-fix']['doc_root'] ) ||
+	empty( $_GLOBALS['php-s-wp']['directory-slash-fix']['path'] )
+) {
+	return; // No need to redirect w/out these.
+}
+
 // Enforce trailing slash on directories because php -S is weird.
 if (
 
-	// Ignore WP CLI.
-	! defined( 'WP_CLI' ) &&
+	// For it to be a directory, it has to exist.
+	file_exists( $_GLOBALS['php-s-wp']['directory-slash-fix']['real_path'] ) &&
 
-	// If it's a directory you're calling...
-	isset( $_SERVER["REQUEST_URI"] ) &&
-	is_dir( __DIR__ . $_SERVER["REQUEST_URI"] ) &&
+	// If you aren't requesting a direct file (it must be a directory or pretty permalink)...
+	! is_file( $_GLOBALS['php-s-wp']['directory-slash-fix']['real_path'] ) &&
 
 	// And it does not have a trailing slash...
-	'/' !== $_SERVER["REQUEST_URI"][-1]
+	'/' !== $_GLOBALS['php-s-wp']['directory-slash-fix']['path'][-1]
 ) {
 
-	// Re-direct to the directory with a trailing slash.
-	header( "Location: {$_SERVER["REQUEST_URI"]}/");
+	if ( ! empty( $_GLOBALS['php-s-wp']['directory-slash-fix']['query'] ) ) {
+
+		// Add the trailing slash w/ the query.
+		header( "Location: {$_GLOBALS['php-s-wp']['directory-slash-fix']['path']}/?{$_GLOBALS['php-s-wp']['directory-slash-fix']['query']}");
+		exit;
+	}
+
+	// Add the trailing slash (no query).
+	header( "Location: {$_GLOBALS['php-s-wp']['directory-slash-fix']['path']}/");
 	exit;
 }
+
+unset( $_GLOBALS['php-s-wp'] );
